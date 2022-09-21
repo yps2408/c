c04226d0869791f64f22f620468a15d19d954ee2
yps2408 <109202422+yps2408@users.noreply.github.com>
2022-09-21 19:31:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// 
+string [] array = new string [10];
+
+string [] FillStringArray(string [] array)
+{
+    for(int i = 0; i < array.Length; i = i + 2)
+    {
+        int temp = new Random().Next(-10000, 10000);
+        array[i] = Convert.ToString(temp);
+    }
+    for(int i = 1; i < array.Length; i = i + 2)
+    {
+       Console.WriteLine("Введите символы элемента строки: ");
+       array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void PrintArray(string [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string [] FillStringArrayAvailableValues(string [] array, int N)
+{
+    string [] result = new string [N];
+    int j = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length < 4)
+        {
+             result[j] = array[i];
+             j++;
+        }
+    }
+    return result;
+}
+
+int NumberOfCharactersPerLine(string [] array)
+{
+    int N = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length < 4)
+        {
+            N++;
+        }
+    }
+    return N;
+}
+
+
+FillStringArray(array);
+PrintArray(array);
+
+int N = NumberOfCharactersPerLine(array);
+Console.WriteLine(N);
+
+string [] newArray = FillStringArrayAvailableValues(array, N);
+PrintArray(newArray);
+
